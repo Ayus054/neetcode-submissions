93129e5233d829f0e395b712d2dca0a7cd0b3c96
Ayus054 <44651732+Ayus054@users.noreply.github.com>
2026-05-09 23:31:46 +0530

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    bool pal(string s,int i,int j)
+    {
+        while(i<j)
+        {
+            if(s[i]!=s[j])
+            return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        int ml=0;
+        int st,e;
+        for(int i=0;i<s.size();i++)
+        {
+            for(int j=0;j<s.size();j++)
+            {
+                if(pal(s,i,j))
+                {
+                    if(ml<j-i+1)
+                    {
+                        st=i;
+                        e=j;
+                        ml=j-i+1;
+                    }
+                }
+            }
+        }
+        return s.substr(st,ml);
+    }
+};
