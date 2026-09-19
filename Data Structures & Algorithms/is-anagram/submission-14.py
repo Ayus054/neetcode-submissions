552a8a4a89b6efbc47215fdddb12dbf21097500d
Ayus054 <44651732+Ayus054@users.noreply.github.com>
2026-09-19 12:18:01 +0530

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        map={}
+        for ch in s:
+            if ch in map:
+                map[ch]+=1
+            else:
+                map[ch]=1
+        for ch in t:
+            if ch in map:
+                map[ch]-=1
+            else:
+                return False
+
+        for i in map:
+            if map[i]>0:
+                return False
+
+        return True
